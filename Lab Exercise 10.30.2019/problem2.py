df7b58c9383aa1333665e5ad43719e2ebdd8c388
nmessa <nmessa@sau16.org>
2019-10-27 10:19:53 -0400

    Add files via upload

---
+++ Lab Exercise 10.30.2019/problem2.py
@@ -0,0 +1,26 @@
+## Lab Exercise 10/30/2019 Problem 2
+## Author: 
+## Removes the smallest value from the list
+
+def removeMin(lst):
+    #Add code here
+
+
+
+    
+#Code to test the function
+myList = [22,4,3,6,8,5,9,7,5,7]
+print (myList)
+print (removeMin(myList))
+
+myList2 = [22,4,3,6,8,5,9,7,5,7]
+print (myList2)
+print (removeMin2(myList2)) 
+
+## Output
+## [22, 4, 3, 6, 8, 5, 9, 7, 5, 7]
+## [4, 5, 5, 6, 7, 7, 8, 9, 22]
+## [22, 4, 3, 6, 8, 5, 9, 7, 5, 7]
+## [22, 4, 6, 8, 5, 9, 7, 5, 7]
+
+
