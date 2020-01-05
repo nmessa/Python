02e0690062a4615cb3fcc42c7ae87d0d8cd35f56
nmessa <nmessa@sau16.org>
2020-01-05 11:46:07 -0500

    Add files via upload

---
+++ Lab Exercise 1.8.2020/NameInPi/problem1.py
@@ -0,0 +1,30 @@
+## Lab Exercise 1/8/2020 Problem 1
+## Author: 
+## This program will find the location of a name in the first
+## billion digits of Pi
+
+#Assign filename
+##filename = 'pi_million_digits.txt'
+filename = 'pi-billion.txt'
+
+#read in the lines in the file
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+#build the pi string
+pi_string = ''
+#Add code here
+
+
+#Get name from user 
+#Add code here
+
+
+myName = ""
+#Find number version of name and store in myName
+#Add code here
+
+
+###check to see if birthday is in the pi string and print it's index
+
+
